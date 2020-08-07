2f124e23884e28859e8f2e03256cdbad969ecbe9
Catalina Astengo <catalina@nav.com>
2020-08-07 07:49:27 -0600

    initial app

---
+++ lib/elixirconf_pokemon_proto.ex
@@ -0,0 +1,18 @@
+defmodule ElixirconfPokemonProto do
+  @moduledoc """
+  Documentation for `ElixirconfPokemonProto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirconfPokemonProto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixirconf_pokemon_proto_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirconfPokemonProtoTest do
+  use ExUnit.Case
+  doctest ElixirconfPokemonProto
+
+  test "greets the world" do
+    assert ElixirconfPokemonProto.hello() == :world
+  end
+end
